d0c97308e60350ace787230e5418a8ea2529e9b7
Cindy Aristawati <61828037+Cindyawati@users.noreply.github.com>
2021-04-21 21:10:23 +0700

    1st Commit

--- app/src/main/java/com/example/chattingapp/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/chattingapp/Fragments/ChatsFragment.java
@@ -27,8 +27,6 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import java.util.ArrayList;
 import java.util.List;
 
-//test fransnfriends
-//test dari branch cindy
 
 public class ChatsFragment extends Fragment {
 
